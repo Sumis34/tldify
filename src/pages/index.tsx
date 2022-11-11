8e5164255e8e5db6aa2ff2a6be848276aba125bd
Noé <eon.sberk@gmail.com>
2022-11-11 11:22:20 +0100

    seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import { ChangeEventHandler, useEffect, useState } from "react";
 import { HiOutlineExternalLink } from "react-icons/hi";
 import { trpc } from "../utils/trpc";
@@ -84,6 +85,10 @@ const Tldify: NextPage = () => {
         backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%234f66d9' fill-opacity='0.5'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
       }}
     >
+      <Head>
+        <title>TLDify</title>
+        <link rel="icon" type="image/x-icon" href="TLDify_fav.png"></link>
+      </Head>
       <div className="z-10 flex flex-col">
         <h1 className="mb-5 text-center text-7xl font-bold text-white">
           TLDify
